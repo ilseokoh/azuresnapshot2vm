b53bdd798a0a2c4c800dbb60a01509256b411a53
Il Seok Oh <iloh@microsoft.com>
2017-03-13 13:43:39 +0900

    Powershell 완료

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# azuresnapshot2vm
-Powershell script for Azure snapshot to managed disk and virtual machine
+# Azure Snapshot으로 VM 생성
+Azure의 Managed Disk는 Snapshot을 만들어 특정 시점의 백업을 만들 수 있습니다. Snapshot을 만드는 방법은 ["관리 스냅숏을 사용하여 Azure Managed Disk로 저장된 VHD 복사본 만들기"](https://docs.microsoft.com/ko-kr/azure/virtual-machines/virtual-machines-windows-snapshot-copy-managed-disk)를 참조 바랍니다. 
+
+만들어진 Snapshot에서 다시 Managed Disk를 만들 수 있고 이 Managed Disk를 이용하여 VM을 생성할 수 있습니다. 이 기능은 아직 (2017-03-13 현재) Azure Portal에서 지원하지 않는 기능이므로 Powershell을 이용하거나 Azure CLI를 사용해야 합니다. 
+
+PowerShell 작업은 4단계로 진행됩니다. 
+1. Snapshot / 가상네트워크 / 서브넷 가져오기 
+1. Public IP / NIC(Network Interface Card) 만들기 
+1. Snapshot에서 Managed Disk 만들기 
+1. VM 만들기
+
+* PowerShell
+* Azure CLI
+
+PowerShell로 진행하려면 
+Azure PowerShell Resource Manager modules의 최신버전이 필요합니다. (2017-03-13 현재 3.7.0) 설치 방법은 1) [MSI 설치파일](https://github.com/Azure/azure-powershell/releases/download/v3.7.0-March2017/azure-powershell.3.7.0.msi) 2) [Microsoft Web Platform Installer](http://aka.ms/webpi-azps) 3) Windows PowerShell을 관리자 모드로 실행하고 > Install-Module AzureRM 실행 
+
+Azure CLI로 진행하려면 
+Azure CLI 최신버전이 필요합니다. 설치방법은 [Azure CLI 설치](https://docs.microsoft.com/ko-kr/azure/xplat-cli-install)문서를 참조 바랍니다. 
---
+++ powershell/snapshot2vm.ps1
@@ -0,0 +1,59 @@
+﻿# 잘 작동되고 있는 VM에서 OS 디스크를 Snapshot으로 만들어서 특정 시점의 백업을 만든 후 
+# Snapshot에서 다시 VM을 생성해야 할 때 사용하는 스크립트 
+# VM을 생성할 때 기존 리소스 그룹과 가상네트워크를 사용함. 
+# 특히 VM에서 이미지를 생성하기 전에 Snapshot으로 백업 후 Restore 할 때 사용 가능 
+
+# 파라미터 설정
+$location = "koreacentral"
+$rgName = "ManagedDiskGroup"
+$snapshotRGName = 'SnapshotGroup'
+$snapshotName = 'VM1-snapshot00'
+$vmName = 'VM2'
+
+$vnetName = "ManagedDiskGroup-vnet"
+$subnetName = "default"
+
+# Snapshot 가져오기 
+$snapshot = Get-AzureRmSnapshot -SnapshotName $snapshotName -ResourceGroupName $snapshotRGName
+# 가상네트워크 가져오기
+$vnet = Get-AzureRmVirtualNetwork -ResourceGroupName $rgName -Name $vnetName
+# 가상네트워크 서브넷 가져오기 
+$subnetConfig = Get-AzureRmVirtualNetworkSubnetConfig -VirtualNetwork $vnet -Name $frontendSubnetName
+
+
+# Public IP 만들기 
+$pipName = $vmName + "PIP"
+$pip = New-AzureRmPublicIpAddress -Name $pipName -ResourceGroupName $rgName -Location $location -AllocationMethod Dynamic
+# 새로운 Network Interface Card 만들기 
+$nicName = $vmName + "NIC"
+$nic = New-AzureRmNetworkInterface -Name $nicName -ResourceGroupName $rgName -Location $location -SubnetId $subnetConfig.Id -PublicIpAddressId $pip.Id
+
+# Snapshot에서 Managed Disk 만들기
+$diskType = 'PremiumLRS'
+$managedDiskCreateOption = 'Copy' 
+$diskName = $vmName + "-osdisk"
+$diskConfig = New-AzureRmDiskConfig -AccountType $diskType -Location $location -CreateOption $managedDiskCreateOption -SourceResourceId $snapshot.Id
+$osDisk = New-AzureRmDisk -DiskName $diskName -Disk $diskConfig -ResourceGroupName $rgName
+
+# 여기까지가 Snapshot에서 Managed Disk 만들기 
+# -------------------------------------
+
+# 새로운 VM 만들기 위한 준비 
+$vmSize = 'Standard_DS2_v2' # 'StandardLRS'
+$diskCreateOption = 'Attach'
+$vm = New-AzureRmVMConfig -VMName $vmName -VMSize $vmSize
+$vm = Add-AzureRmVMNetworkInterface -VM $vm -Id $nic.Id
+$vm = Set-AzureRmVMOSDisk -VM $vm -Name $diskName -ManagedDiskId $osDisk.Id -CreateOption $diskCreateOption -Windows -Caching ReadWrite
+
+# 드디어 VM 만들기 
+New-AzureRmVM -ResourceGroupName $rgName -Location $location -VM $vm -Verbose
+
+# 완료 
+
+
+
+
+# ---------------------------------------
+# 아래는 참조용 
+# 특정 지역에서 사용가능한 VM 사이즈 리스트 
+Get-AzureRmVMSize -Location $location
